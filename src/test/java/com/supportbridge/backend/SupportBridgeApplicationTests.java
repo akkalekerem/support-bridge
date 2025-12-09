58199b8d2aae4f8b9b03f6ef2c3382740553e060
Kerem Akkale <keremakkale@gmail.com>
2025-12-09 12:44:42 +0300

    Proje açıldı

---
+++ src/test/java/com/supportbridge/backend/SupportBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supportbridge.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportBridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
